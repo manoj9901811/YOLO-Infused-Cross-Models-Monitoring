fd3780cf2c9e855cda67a8bb4a2ec9993be9db4b
Manoj Somanna <133636396+manoj9901811@users.noreply.github.com>
2025-08-17 12:21:01 +0530

    Update settings.py

--- surveillance_project/settings.py
+++ surveillance_project/settings.py
@@ -88,8 +88,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'manojsomanna990@gmail.com'
-EMAIL_HOST_PASSWORD = 'czzfcwayqsiwgysg'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 
